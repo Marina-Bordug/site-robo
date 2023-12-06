4298a5914b163b8100fac129f7997c664eee0222
Marifei <116741086+Marina-Bordug@users.noreply.github.com>
2023-12-06 20:11:34 +0300

    Add files via upload

--- prroject/script.js
+++ prroject/script.js
@@ -1,1 +1,22 @@
-console.log(1);
+
+// console.log(list)
+// let d = document.querySelector(".titles");
+
+// d.innerHTML = list[0]["text"]
+
+let content = document.querySelector(".topic-content")
+console.log(list)
+let links = document.querySelectorAll(".small-topic")
+links.forEach(
+    (link)=>{
+        console.log(link.dataset.id)
+        
+        link.addEventListener("click", event => {
+            content.innerHTML = list[link.dataset.id]["text"]
+        });
+    }
+)
+
+
+
+
